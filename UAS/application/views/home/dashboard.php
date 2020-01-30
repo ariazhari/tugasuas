a36eed53515880e7239da8e647d3c4078f12ac73
unknown <aazhari431@gmail.com>
2020-01-30 20:07:22 +0700

    first commit

---
+++ UAS/application/views/home/dashboard.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+     <head>
+<html></html>
+<link rel="stylesheet" href="<?= base_url()?>templates/style.css">
+
+</head>
+<body class="">
+<div class="container text-center">
+
+
+<h1><marquee>BIODADTA</marquee></h1>
+<img src="<?= base_url()?>templates/dist/img/credit/a.jpg" style="width:200px" alt="Responsive image">
+<br><br><br>"
+<p>NAMA:ARI AZHARI</p>
+<P>ALAMAT:TAMBUN,MONGGAS,KOPANG,LOTENG,NTB</P>
+<P>JENIS KELAMIN:LAKI-LAKI</P>
+<P>HOBI:MANCING</P>
+<br>
+<h4><marquee direction="right">STMIKA LOMBOK</marquee></h4>
+<h4><marquee direction="left">STMIK LOMBOK</marquee></h4>
+
+          <nav>
+               <ul>
+                    <li>PENDIDIKAN
+                              <ul>
+                                   <a href=""><li>SDN 1 MONGGAS</li></a>
+                                   <a href=""><li>MTs DARUL HAMIDY NW IWAN</li></a>
+                                   <a href=""><li>SMA ATTOHIRIYAH AL FADILIYAH</li></a>
+                                   <a href=""><li>STMIK LOMBOK</li></a>
+                              </ul>
+
+                         </li>
+                        <li>IDENTITAS
+                             <ul>
+                                  <a href=""><li>NAMA:Ari Azhari</li></a>
+                                  <a href=""><li>ALAMAT:Mongggas</li></a>
+                                  <a href=""><li>TTL:30 APRIL 1998</li></a>
+                             </ul>
+                         
+                        </li>
+                    <li>CALL
+                     <ul>
+                         <a href=""><li>TELKOMSEL(WA):+6285337689527</li></a>
+                         <a href=""><li>XL:+6281779672783</li></a>
+                         <a href=""><li>EMAIL:ariazhri@gmail.com</li></a>
+                         <a href="http//www.facebook.com"facebook="http//www.facebook.com"fb><li>facebook:Ary Azhari</li></a>
+                    </ul>
+                    </li>
+               </ul>
+          </nav>
+
+</div>
+</body>
+
+<head>
+<link rel="<?= base_url()?>stylesheet" href="<?= base_url()?>system.css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><Page Title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?= base_url()?>main.css">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+    <script src="main.js"></script>
+</head>
+<body>
+    
+    <ul>
+        <li><a><i class="fa fa-facebook-official" aria-hidden="true"></i> </a></li>
+        <li><a><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+        <li><a><i class="fa fa-google-plus-official" aria-hidden="true"></i></a></li>
+        <li><a><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+        <li><a><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+        <li><a><i class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
+        <li><a><i class="fa fa-phone" aria-hidden="true"></i></a></li>
+        <li><a><i class="fa fa-commenting-o" aria-hidden="true"></i></a></li>
+        <li><a><i class="fa fa-comments-o" aria-hidden="true"></i></a></li>
+        <li><a><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+    </ul>
